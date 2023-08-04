800cff85248256241091e88a83f9fb0255ba997b
chiki-brik <afedyushina9909@gmail.com>
2023-08-04 15:15:22 +0300

    changed gulp, adaptation. project is ready

---
+++ dist/js/script.js
@@ -0,0 +1,205 @@
+// БИБЛИОТЕКА JQUERY
+// $(document).ready(function(){ // загружать слайдер только тогда, когда документ полностью готов. $ - это и есть библиотека jQuery
+//     $('.carousel__inner').slick({
+//         //dots: true,
+//         //infinite: true,
+//         speed: 1200, //ms
+//         //slidesToShow: 1,
+//         //adaptiveHeight: true,
+//         prevArrow: '<button type="button" class="slick-prev"><img src="img/catalogue/arrows/img_left_ar.png"></button>', 
+//         nextArrow: '<button type="button" class="slick-next"><img src="img/catalogue/arrows/img_right_ar.png"></button>',
+//         //arrows: false
+//         //fade: true, //проявление картинки
+//         //cssEase: 'linear' //тип проявления, здесь равномерное
+//         // autoplay: true,
+//         // autoplaySpeed: 2000
+//         responsive: [ // для адаптации
+//             {
+//                 breakpoint: 992, // на каком промежутке работают правила (от 0 до 1024), все что выше 1024, будет работать по правилам, описанным ВЫШЕ
+//                 settings: {
+//                   dots: true,
+//                   arrows: false
+//                 }
+//             }
+//         ]
+       
+//       });
+//   });
+
+// TINY-SLIDER
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    speed: 1200,
+    controls: false,
+    nav: false,
+    autoHeight:true,
+    //nested: true,
+    responsive: {
+        // 640: {
+        //   edgePadding: 20,
+        //   gutter: 20,
+        //   items: 2
+        // },
+        // 700: {
+        //   gutter: 30
+        // },
+        // 991: {
+        //   items: 3
+        // }
+      }
+  });
+
+document.querySelector('.prev').addEventListener('click', function () { 
+    slider.goTo('prev'); 
+}); 
+
+document.querySelector('.next').addEventListener('click', function () { 
+    slider.goTo('next'); 
+});
+
+// изменения отображения на странице элементов в зависимости от выбранного таба
+$(document).ready(function(){
+    $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+        $(this) //ссылается на элемент, на который только что нажали
+          .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+          .closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+      });
+
+    // $('.catalogue-item__link').each(function(i) {
+    //     $(this).on('click', function(e) { // е тут обязательно для регулировки станд.поведения браузера
+    //         e.preventDefault(); //убираем стандартное поведение браузера, чтобы при клике на ссылку, не отправляло в начало страницы
+    //         $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+    //         $('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+    //     })
+    // })   
+
+// смена информации внутри элемента при нажатии на раскр."меню"
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) { // е тут обязательно для регулировки станд.поведения браузера
+                e.preventDefault(); //убираем стандартное поведение браузера, чтобы при клике на ссылку, не отправляло в начало страницы
+                $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+                //$('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+                $('.catalogue-item__reverse').eq(i).toggleClass('catalogue-item__reverse_active');
+            })
+        }) 
+    };
+
+    toggleSlide('.catalogue-item__link');
+    toggleSlide('.catalogue-item__back');
+
+     // Modal - должна быть подключена jQuery
+    $('[data-modal=consultation]').on('click', function() { 
+        $('.overlay, #consultation').fadeIn('slow'); //fadeOut - плавное скрытие элементов со страницы
+    }); //квадратные скобки используются для атрибутов 
+
+    $('.modal__close').on('click', function(){
+        $('.overlay, #consultation, #thnx, #order').fadeOut('slow');
+    });
+
+    // убираем - будет описано ниже
+    // $('.button_mini').on('click', function() {
+    //     $('.overlay, #order').fadeIn('slow');
+    // });
+
+    // чтобы в модальное окно подставлялись разные названия пульсометров
+    $('.button_mini').each(function(i) {
+        $(this).on('click', function() {
+            $('#order .modal__descr').text($('.catalogue-item__subtitle').eq(i).text()); //если оставить text() пустым, то текст будет получаться, если заполнить - передаваться
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    // валидация форм! с помощью плагина
+    function validateForms(form) {
+        $(form).validate({ // работает только на первом элементе данного селектора, поэтому нужно воспользоваться вложенностью уникальных айди, либо просто айди форм
+            rules: {
+                name: {
+                    required: true,
+                    minlength: 2
+                  },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true
+                }
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите свое имя",
+                    minlength: jQuery.validator.format("Длина имени не должна быть менее {0} символов!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                  required: "Пожалуйста, введите свою почту",
+                  email: "Некорректный адрес электронной почты!"
+                }
+              }
+        });
+    };
+
+    validateForms('#order form');
+    validateForms('#consultation-form');
+    validateForms('#consultation form');
+
+    $('input[name=phone]').mask("+7 (999) 999-99-99");
+
+    // отправка писем с формы
+    $('form').submit(function(e) { // после того, как форма сабмитится - все данные с нее будут отправляться. e- even
+        e.preventDefault(); // чтобы после отправки формы автоматически не было перезагрузки страницы
+        if (!$(this).valid()) { // если форма не прошла валидацию - данные не отправятся на сервер
+            return;
+        }
+        $.ajax({ // метод ajax - метод внутри jQuery
+            type: "POST", // отправка данных, или взятие данных с сервера
+            url: "mailer/smart.php", // куда будут отправляться данные
+            data: $(this).serialize() // что за данные отправляются на сервер - данные с формы, к которым применен метод serialize
+        }).done( function () { //что происходит после выполнения этой операции
+            $(this).find("input").val(""); // очищаем форму после отправки
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thnx').fadeIn();
+
+            $('form').trigger('reset'); //обновление форм
+        }); 
+        return false;
+    }); 
+
+    //smooth scroll and pageup
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) { // this - наша страница, смотрим сколько пикселей пользователь промотал
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+
+        // Add smooth scrolling to all links
+    $("a[href='#up']").on('click', function(event) {
+    
+        // Make sure this.hash has a value before overriding default behavior
+        if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+    
+        // Store hash
+        const hash = this.hash;
+    
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+            scrollTop: $(hash).offset().top
+        }, 800, function(){
+    
+            // Add hash (#) to URL when done scrolling (default click behavior)
+            window.location.hash = hash;
+        });
+        } // End if
+    });
+
+    new WOW().init();
+});
+  
--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,13 @@ const sass = require('gulp-sass')(require('sass')); // можно взять с 
 const cleanCSS = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
 const rename = require("gulp-rename");
+const imagemin = require('gulp-imagemin');
+const htmlmin = require('gulp-htmlmin');
 
 gulp.task('server', function() { //server - название задачи
     browserSync({ 
         server: {
-            baseDir: "src" // откуда будет запускаться сервер. В этой папке будет искаться файл index.html и запускаться
+            baseDir: "dist" //"src" // откуда будет запускаться сервер. В этой папке будет искаться файл index.html и запускаться
         }
     });
     gulp.watch("src/*.html").on('change', browserSync.reload); // обновление страницы при изменении html-файла
@@ -20,12 +22,46 @@ gulp.task('styles', function() {
     .pipe(rename({suffix: '.min', prefix: ''}))
     .pipe(autoprefixer())
     .pipe(cleanCSS({compatibility: 'ie8'}))
-    .pipe(gulp.dest("src/css")) // куда нужно переместить файл-результат
+    //.pipe(gulp.dest("src/css")) // куда нужно переместить файл-результат
+    .pipe(gulp.dest("dist/css"))
     .pipe(browserSync.stream()); // обновление страницы
 });
 
 gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles')); // при обновлении sass\scss файлов будет запускаться задача style, которая эти файлы будет компилировать и обновлять странцу
-})
+    gulp.watch("src/sass/**/*.+(scss|sass|css)", gulp.parallel('styles')); // при обновлении sass\scss файлов будет запускаться задача style, которая эти файлы будет компилировать и обновлять странцу
+    gulp.watch("src/*.html").on('change', gulp.parallel('html')); // при обновлении html-файла запускаем задачу по его минимизации и перенесению в папку dist
+});
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles')); // задача, которая запускается всегда по-умолчанию. Обязательное имя default. Она будет запускать все нужные нам задачи
+gulp.task('html', function() { // минимизация html-кода и перемещение в папку dist
+    return gulp.src("src/*.html")
+        .pipe(htmlmin({ collapseWhitespace: true }))
+        .pipe(gulp.dest("dist/"));
+});
+
+gulp.task('scripts', function() { // перемещение в папку dist всех скриптов
+    return gulp.src("src/js/**/*.js")
+        .pipe(gulp.dest("dist/js"));
+});
+
+gulp.task('fonts', function() { // перемещение в папку dist всех шрифтов
+    return gulp.src("src/fonts/**/*")
+        .pipe(gulp.dest("dist/fonts"));
+});
+
+gulp.task('icons', function() { // перемещение в папку dist всех иконок
+    return gulp.src("src/icons/**/*")
+        .pipe(gulp.dest("dist/icons"));
+});
+
+gulp.task('mailer', function() { // перемещение в папку dist расширения для отправки форм
+    return gulp.src("src/mailer/**/*")
+        .pipe(gulp.dest("dist/mailer"));
+});
+
+gulp.task('images', function() { // перемещение в папку dist всех картинок
+    return gulp.src("src/img/**/*")
+        .pipe(imagemin())  //сжатие изображений
+        .pipe(gulp.dest("dist/img"));
+});
+
+gulp.task('default', gulp.parallel('watch', 'server', 'styles', 'scripts', 'fonts', 'icons', 'html', 'mailer', 'images')); // задача, которая запускается всегда по-умолчанию. Обязательное имя default. Она будет запускать все нужные нам задачи
